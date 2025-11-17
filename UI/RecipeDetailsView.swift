475476a2d8ec9df8cefba105d8e63355d42338a5
Andriantsialonina <andriantsialonina@sct-21-eba7-1371.univ-rouen.fr>
2025-11-17 16:09:06 +0100

    (fix): ajout du titre de la navigation

--- UI/RecipeDetailsView.swift
+++ UI/RecipeDetailsView.swift
@@ -14,9 +14,26 @@ struct RecipeDetailsView : View {
     
     var body: some View {
         VStack{
-            Text(recipe.name)
+            HStack{
+                VStack(alignment : .leading){
+                    Text("Prep Time : \(recipe.prep_time) mins")
+                    Text("Cook Time : \(recipe.cook_time) mins")
+                }
+                Button("Edit recipe"){
+                    
+                }
+                .buttonStyle(.bordered)
+                .background(Color.yellow.opacity(0.3))
+            }
         }
+        .frame(maxWidth : .infinity, alignment: .leading)
+        .navigationTitle(recipe.name)
+        .navigationBarTitleDisplayMode(.large)
     }
+}
+
+#Preview {
+    RecipeDetailsView(recipe: .constant(Recipe(photo_name : "clafoutis", name : "Clafoutis", prep_time : 15, cook_time : 25, serving : 8, ingredients : [Ingredient(name : "sucre", quantity : 180, unit : "g" ), Ingredient(name : "poires", quantity : 6, unit : " " ), Ingredient(name : "oeufs" , quantity : 6, unit : " " ), Ingredient(name : "farine", quantity : 125, unit : "g" ), Ingredient(name : "crème liquide", quantity : 50, unit : "cl" ), Ingredient(name : "lait", quantity : 25, unit : "cl" ), Ingredient(name : "beurre", quantity : 10, unit : "cl" )], directions : "Fais fondre 200 g de chocolat noir avec 100 g de beurre. Dans un saladier, bats trois œufs avec 100 g de sucre jusqu’à ce que le mélange devienne mousseux. Ajoute ensuite 80 g de farine. Incorpore le chocolat fondu puis mélange jusqu’à obtenir une pâte bien lisse. Verse la préparation dans un moule beurré ou tapissé de papier cuisson. Fais cuire au four à 180 °C pendant environ vingt à vingt-cinq minutes. Laisse tiédir avant de démouler pour un gâteau fondant et savoureux.")))
 }
 
 
